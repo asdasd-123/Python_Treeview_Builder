b6eaa642734787094174741d4f092a503678b793
JPJ <jack.pj@live.co.uk>
2019-02-07 22:49:41 +0000

    Added kwarg support and test data

--- Example.py
+++ Example.py
@@ -2,11 +2,50 @@ from TreeBuild import TreeBuild
 import tkinter as tk
 import tkinter.ttk as ttk
 
+search_headings = ("ID", "Name", "Chip No. ", "Vaccinated")
+search_data = (
+    ("01", "Cookie", "7493732", "42005"),
+    ("02", "Gibbie", "7342152", "42038"),
+    ("03", "Tinkerbelle", "1681023", "42071"),
+    ("04", "Wispa", "6369268", "42104"),
+    ("05", "Pebbles", "4362464", "42137"),
+    ("06", "Tatsiana", "5674374", "42170"),
+    ("07", "Cookiea", "9076052", "42203"),
+    ("08", "Gibbiea", "6237524", "42236"),
+    ("09", "Tinkerbellea", "6159266", "42269"),
+    ("10", "Wispaa", "1521653", "42302"),
+    ("11", "Pebblesa", "8150588", "42335"),
+    ("12", "Tatsianaa", "1505322", "42368"),
+    ("13", "Cookieaa", "4445858", "42401"),
+    ("14", "Gibbieaa", "6390976", "42434"),
+    ("15", "Tinkerbelleaa", "1968907", "42467"),
+    ("16", "Wispaaa", "1501928", "42500"),
+    ("17", "Pebblesaa", "3430556", "42533"),
+    ("18", "Tatsianaaa", "6453150", "42566"),
+    ("19", "Cookieaaa", "3750061", "42599"),
+    ("20", "Gibbieaaa", "1742287", "42632"),
+    ("21", "Tinkerbelleaaa", "7138771", "42665"),
+    ("22", "Wispaaaa", "4020316", "42698"),
+    ("23", "Pebblesaaa", "6514509", "42731"),
+    ("24", "Tatsianaaaa", "1438670", "42764"),
+    ("25", "Cookieaaaa", "1151414", "42797"),
+    ("26", "Gibbieaaaa", "8535522", "42830"),
+    ("27", "Tinkerbelleaaaa", "4106620", "42863"),
+    ("28", "Wispaaaaa", "4390267", "42896"),
+    ("29", "Pebblesaaaa", "2554880", "42929"),
+    ("30", "Tatsianaaaaa", "7180502", "42962"),
+    ("31", "Cookieaaaaa", "9735914", "42995")
+    )
+
 root = tk.Tk()
 
 main_frame = ttk.Frame(root, width="500", height="500")
 main_frame.pack(expand=True, fill="both")
 
-Tree1 = TreeBuild(main_frame)
+Tree1 = TreeBuild(
+    main_frame, 
+    # search=True, 
+    data=search_data,
+    headings=search_headings)
 
 root.mainloop()
--- TreeBuild.py
+++ TreeBuild.py
@@ -2,9 +2,21 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 class TreeBuild:
-    def __init__(self, parent):
+    def __init__(self, parent, search=False, **kwargs):
+        """parent - """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            print("self." + key + " = " + str(value))
+        
         self.parent = parent
-        self._build_frame()
+        self.search = search
+        # Saving the below example incase I need to access any kwargs
+        # self.search = getattr(self, "search", None)
+        if self.search == True:
+            self._build_search_frame()
     
-    def _build_frame(self):
-        print("test")
+    def _build_tree_frame(self):
+        print("tree frame")
+    
+    def _build_search_frame(self):
+        print("search frame")
